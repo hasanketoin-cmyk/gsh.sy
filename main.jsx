dda7c9502b06c7cb537e8bdb171ad3d28ef5419a
hasanketoin-cmyk <hasan.ketoin@gmail.com>
2025-09-01 01:33:05 +0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import SmartHomeSite from './smart-home-site.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SmartHomeSite />
+  </React.StrictMode>
+)
---
+++ smart-home-site.jsx
@@ -0,0 +1,2 @@
+// Copy the SmartHomeSite component code you wrote in canvas here.
+// For brevity, not repeating full content.
